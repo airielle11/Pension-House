dc9f423bc49505719cdba2ed4821c200a28d44da
elruin <arnadoairiellemarie@gmail.com>
2024-11-30 17:30:26 +0800

    updates testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,7 @@ import router from './routes';
 import ThemeCustomization from './themes';
 import ScrollTop from './components/ScrollTop'; 
 
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-import ProtectedRoute from './components/ProtectedRoute'; 
-import NotFound from './pages/404'
-import Dashboard from './pages/dashboard/index';
-import Login from './pages/authentication/login'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom" 
 
 // ==============================|| APP - THEME, ROUTER, LOCAL ||============================== //
 
@@ -20,21 +16,7 @@ function Logout() {
 }
 
 export default function App() {
-  return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route 
-    //       path='/'
-    //       element={
-    //         <ProtectedRoute>
-    //           <Dashboard />
-    //         </ProtectedRoute>
-    //       }
-    //     /> 
-    //     <Route path='/pages/authentication/login' element={<Login />} />
-    //     <Route path='*' element={<NotFound />}></Route>
-    //   </Routes>
-    // </BrowserRouter>
+  return ( 
      <ThemeCustomization>
      <ScrollTop>
        <RouterProvider router={router} />
